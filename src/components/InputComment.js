6a9d36a9a0f18701fd5055f6c0229d89d74b98ae
youngdong82 <nfs82young@gmail.com>
2022-08-06 09:07:03 +0900

    ✨ InputComment.js

---
+++ src/components/InputComment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function InputComment() {
+  return (
+    <div className="InputComment">
+      <textarea />
+      <button>등록</button>
+    </div>
+  );
+}
+
+export default InputComment;
