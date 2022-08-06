83c4ff58fc9458d23aaa6f01f01d45f2fb03f2bc
jinaSE0 <tiffany6771@gmail.com>
2022-08-06 12:07:15 +0900

    mainpage-view

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../components/layout/Layout"
+import List from "../components/list/List"
+
+
+const MainPage = () => {
+
+  return(
+   <>
+   <Layout>
+      <List/>;
+   </Layout>
+   </>
+  );
+};
+
+export default MainPage;
