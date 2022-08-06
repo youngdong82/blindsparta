fb7658b3d750b40b845be71612a4a5dc9ad15397
youngdong82 <nfs82young@gmail.com>
2022-08-06 09:09:44 +0900

    ✨ react-router-dom setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from React;
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import BlindBoard from './routers/BlindBoard';
 
 function App() {
   return (
     <div className="App">
       열심히 해보아요.
+      <Router>
+        <Routes>
+          <Route path='/dongjak' element={<BlindBoard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
