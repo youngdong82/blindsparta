fada4fc2dd131789c97a8094ed6b27f2d2804ba9
jinaSE0 <tiffany6771@gmail.com>
2022-08-09 19:36:19 +0900

    리덕스 구현하다가 실패해서 주석처리해놓음

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,15 +2,30 @@
 import React from "react";
 import "./style.css";
 import List from "../list/List";
+// import { DummyData } from "../../dummyData/dummyCamp";
 
 
 
 const Layout = () =>{
+
+  // const [plus, setPlus] = useState([
+  //   {
+  //     id :
+  //     더미더미
+  //   }
+  // ])
+
   return <div className="layout">
     <h2 className="title">캠프 목록</h2>
     <hr className="bar"></hr>
     <List></List>
+    {/* <button className="plusbutton" onClick={() => {
+        setPlus([...List])
+    }}> + </button> */}
   </div>;
 };
+
+
+
 
 export default Layout;
