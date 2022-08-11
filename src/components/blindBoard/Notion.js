7e8d1f50f9cff93343b5e45a2e005ceab5d7428a
caffesale <pglibrary80@gmail.com>
2022-08-11 12:24:21 +0900

    Fix 노션 삭제에 어드민 권한 확인
    -----------------------------------------------
    [체크리스트]
    
        제목 첫 글자는 대문자로 작성했나요?
        제목은 명령문으로 작성했나요?
        제목 끝에 마침표 금지
        제목과 본문을 한 줄 띄워 분리하기
        본문에 여러 줄의 메세지를 작성할 때는 "~"로 구분하기
    -----------------------------------------------

--- src/components/blindBoard/Notion.js
+++ src/components/blindBoard/Notion.js
@@ -8,6 +8,9 @@ function Notion({data, deleteNotion}) {
   const dispatch = useDispatch();
   const comments_redux = useSelector((state) => state.commentReducer.commentList);
   const [comments, setComments] = useState([]);
+  // 삭제 권한 확인
+  const isAdmin = useSelector(state => state.signReducer.admin);
+
   useEffect(() => {
     dispatch(loadCommentFB())
   },[])
@@ -52,7 +55,10 @@ function Notion({data, deleteNotion}) {
           <span>작성자: {data.user_id}</span>
           <div>
             <button onClick={toggleLike}>추천 수 : {data.like + like}</button>
-            <button onClick={() => {deleteNotion(data.title)}}>삭제</button>
+            { isAdmin
+            ? <button onClick={() => {deleteNotion(data.title)}}>삭제</button>
+            : <></>
+            }
           </div>
       </div>
       <p>{data.description}</p>
